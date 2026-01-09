9c926b06982f607700f8073c2e912facd16bf183
Jairo Rodriguez <jairodri.a05@gmail.com>
2026-01-09 03:04:29 +0000

    mejoras front pantalla home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import {
+ /* import {
   IonPage,
   IonContent,
   IonButton,
@@ -37,6 +37,82 @@ const Home: React.FC = () => {
           Salir
         </IonButton>
 
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
+*/
+
+import {
+  IonPage,
+  IonContent,
+  IonButton,
+  IonIcon,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonButtons,
+  IonMenuButton
+} from '@ionic/react';
+import { personCircleOutline, notificationsOutline } from 'ionicons/icons';
+import { useHistory } from 'react-router';
+import './Home.css';
+
+const Home: React.FC = () => {
+  const history = useHistory();
+
+  const salir = () => {
+    history.replace('/login');
+  };
+
+  return (
+    <IonPage>
+      {/* Header */}
+      <IonHeader>
+        <IonToolbar className="home-toolbar">
+          <IonTitle>Sistema de Reservas</IonTitle>
+          <IonButtons slot="end">
+            <IonIcon icon={notificationsOutline} className="home-icon" />
+            <IonMenuButton />
+          </IonButtons>
+        </IonToolbar>
+      </IonHeader>
+
+      <IonContent>
+        <div className="home-container">
+          {/* Logo ESPOL */}
+          <div className="home-logo">
+            <h1>ESPOL</h1>
+          </div>
+
+          {/* Bienvenida */}
+          <div className="home-welcome">
+            <h2>Bienvenido de nuevo</h2>
+
+            <div className="home-avatar">
+              <IonIcon icon={personCircleOutline} />
+            </div>
+
+            <h1>USUARIO</h1>
+          </div>
+
+          {/* Botones */}
+          <div className="home-actions">
+            <IonButton expand="block" onClick={() => history.push('/reservas')}>
+              Reservar
+            </IonButton>
+
+            <IonButton expand="block" onClick={() => history.push('/mis-solicitudes')}>
+              Ver solicitudes
+            </IonButton>
+
+            <IonButton expand="block" fill="outline" color="danger" onClick={salir}>
+              Salir
+            </IonButton>
+          </div>
+        </div>
       </IonContent>
     </IonPage>
   );
